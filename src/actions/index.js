7a1dd422904d077aee8a842b585767e90683be87
seoorient <seoorient@gmail.com>
2020-06-11 13:09:21 +0300

    Finished re-store books

---
+++ src/actions/index.js
@@ -0,0 +1,56 @@
+
+const booksLoaded = (newBooks) => {
+  return {
+    type: 'FETCH_BOOKS_SUCCESS',
+    payload: newBooks
+  };
+};
+const booksRequested = () => {
+  return {
+    type: 'FETCH_BOOKS_REQUEST'
+  }
+}
+const booksError = (error) => {
+  return {
+    type: 'FETCH_BOOKS_FAILURE',
+    payload: error
+  }
+}
+export const bookAddedToCart = (bookId) => {
+  return {
+    type: 'BOOK_ADDED_TO_CART',
+    payload: bookId
+  }
+}
+export const bookRemovedFromCart = (bookId) => {
+  return {
+    type: 'BOOK_REMOVED_FROM_CART',
+    payload: bookId
+  }
+}
+export const allBookRemovedFromCart = (bookId) => {
+  return {
+    type: 'ALL_BOOK_REMOVED_FROM_CART',
+    payload: bookId
+  }
+}
+
+//asynchrony without thunk
+const fetchBooksOld = (bookstoreService, dispatch) => () => {
+    dispatch(booksRequested())
+    bookstoreService.getBooks()
+    .then((data) => dispatch(booksLoaded(data)))
+    .catch((err) => dispatch(booksError(err)))      
+}
+
+//using thunk
+const fetchBooks = (bookstoreService) => () => (dispatch) => {
+  dispatch(booksRequested())
+  bookstoreService.getBooks()
+  .then((data) => dispatch(booksLoaded(data)))
+  .catch((err) => dispatch(booksError(err)))
+}
+
+export {
+  fetchBooks
+};
